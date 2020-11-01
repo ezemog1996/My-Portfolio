c04974b05f6b40b8d7baedeaa578e35e2f0ceab2
Enrique Gomez <enrique_g_gomez@hotmail.com>
2020-11-01 02:15:29 -0500

    Added some screenshots of small projects to my portfolio page and used javascript to redirect the user to those app that they click on

---
+++ assets/script/portfolioScript.js
@@ -0,0 +1,11 @@
+document.querySelector("#planner").addEventListener("click", function() {
+    window.location.replace("https://ezemog1996.github.io/Work-Day-Scheduler/");
+})
+
+document.querySelector("#quiz").addEventListener("click", function() {
+    window.location.replace("https://ezemog1996.github.io/Superhero-Quiz/");
+})
+
+document.querySelector("#password").addEventListener("click", function() {
+    window.location.replace("https://ezemog1996.github.io/Password-Generator/");
+})
